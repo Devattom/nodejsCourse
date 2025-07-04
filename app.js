93e8e563d48ea675eedec4395216f08dfbb38d2d
Rousselin Thomas <98778784+Devattom@users.noreply.github.com>
2025-07-04 13:15:10 +0200

    hello express (#3)
    Co-authored-by: Thomas ROUSSELIN <t.rousselin@mtarget.fr>

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res) => res.send('Hello Express 2'));
+
+app.listen(port, () => console.log('Application démarée !'))
