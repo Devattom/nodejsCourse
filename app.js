a4663ed9aed48f74d0ac42cb55f82756e56032f4
Rousselin <devattom@gmail.com>
2025-07-07 19:43:10 +0200

    crud

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
-const { success } = require('./helper');
+const parser = require('body-parser');
+const { success, getUniqueId } = require('./helper');
 
 let pokemons = require('./mock-pokemon');
 
@@ -10,7 +11,8 @@ const port = 3000;
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
-    .use(morgan('dev'));
+    .use(morgan('dev'))
+    .use(parser.json());
 
 app.get('/', (req,res) => res.send('Hello Express 2'));
 
@@ -26,4 +28,28 @@ app.get('/api/pokemons', (req,res) => {
     res.json(success(message, pokemons));
 })
 
+app.post('/api/pokemons', (req,res) => {
+    const id = getUniqueId(pokemons);
+    const pokemonCreated = {...req.body, ...{id: id, created: new Date()}};
+    pokemons.push(pokemonCreated);
+    const message = `Le pokémon ${pokemonCreated.name} a bien été crée`;
+    res.json(success(message, pokemonCreated));
+})
+
+app.put('/api/pokemons/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const pokemonUpdated = { ...req.body, id: id};
+    const indexToUpdate = pokemons.findIndex(pokemon => pokemon.id === id);
+    pokemons[indexToUpdate] = pokemonUpdated;
+    const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié`;
+    res.json(success(message, pokemonUpdated));
+})
+
+app.delete('/api/pokemons/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id);
+    pokemons = pokemons.filter(pokemon => pokemon.id !== id);
+    const message = `Le pokémone ${pokemonDeleted.name} a bien été supprimé`;
+    res.json(success(message, pokemonDeleted));
+})
 app.listen(port, () => console.log('Application démarée !'))
