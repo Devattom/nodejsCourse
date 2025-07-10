11b44c0bb7dc00fee238205fe667ce9305211973
Thomas ROUSSELIN <t.rousselin@mtarget.fr>
2025-07-10 13:33:02 +0200

    Ajout errors

--- app.js
+++ app.js
@@ -21,4 +21,8 @@ require('./src/routes/createPokemon')(app);
 require('./src/routes/updatePokemon')(app);
 require('./src/routes/deletePokemon')(app);
 
+app.use(({res}) => {
+    const message = 'Impossible de trouver la ressource demandée.';
+    res.status(404).json({message});
+})
 app.listen(port, () => console.log('Application démarée !'))
--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -6,11 +6,20 @@ module.exports = (app) => {
         Pokemon.update(req.body, {
             where: { id: id }
         })
-            .then(_ => {
-                Pokemon.findByPk(id).then(pokemon => {
-                    const message = `Le pokémon ${pokemon.name} a bien été modifié.`
-                    res.json({message, data: pokemon })
-                })
+        .then(_ => {
+            return Pokemon.findByPk(id).then(pokemon => {
+                let message = '';
+                if (pokemon === null) {
+                    message = 'Le pokemon demandé n\'existe pas'
+                    return res.status(404).json({message});
+                }
+                message = `Le pokémon ${pokemon.name} a bien été modifié.`
+                res.json({message, data: pokemon })
             })
+        })
+        .catch(error => {
+            const message = 'Erreur lors de la modification du pokémon'
+            res.status(500).json({message, data: {error}});
+        })
     })
 }
