d0babbb531da9ec56f65a2daf5310730689df732
Rousselin Thomas <98778784+Devattom@users.noreply.github.com>
2025-07-17 13:32:37 +0200

    amélioration requête (#15)
    Co-authored-by: Thomas ROUSSELIN <t.rousselin@mtarget.fr>

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,11 +1,35 @@
-const { Pokemon } = require('../db/sequelize')
+const {Pokemon} = require('../db/sequelize');
+const {Op} = require('sequelize');
 
 module.exports = (app) => {
     app.get('/api/pokemons', (req, res) => {
-        Pokemon.findAll()
+        if (req.query.name) {
+            const name = req.query.name;
+            if (name.length <= 1) {
+                const message = 'Merci de saisir au moins 2 lettres pour votre recherche'
+                return res.status(400).json({message})
+            }
+
+            const limit = parseInt(req.query.limit) || 5;
+            return Pokemon.findAndCountAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${name}%`
+                    }
+                },
+                limit: limit,
+                order: ['name']
+            })
+                .then(({count, rows}) => {
+                    const message = `Il y a ${count} pokémons qui correspondent au terme de recherche ${name}`
+                    res.json({message, data: rows})
+                })
+        }
+
+        Pokemon.findAll({order: ['name']})
             .then(pokemons => {
                 const message = 'La liste des pokémons a bien été récupérée.'
-                res.json({ message, data: pokemons })
+                res.json({message, data: pokemons})
             })
             .catch(error => {
                 const message = `La liste des pokémons n'a âs pu être récupérée.`
